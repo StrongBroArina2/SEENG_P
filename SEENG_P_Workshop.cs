696da9ca09a13135883140f2d064688a21c8b0c3
StrongBroArina2 <arina.lokhovskaya@mail.ru>
2025-06-17 19:54:06 +0300

    GetWorkshop

---
+++ SEENG_P_Workshop.cs
@@ -0,0 +1,23 @@
+﻿namespace SEENG_Core
+{
+    public class WorkshopMod
+    {
+        public string Name { get; }
+        public string ModPath { get; }
+        public string ArcShipSysPath { get; }
+        public string AudioSbcPath { get; }
+
+        public WorkshopMod(string name, string modPath, string arcShipSysPath, string audioSbcPath)
+        {
+            Name = name;
+            ModPath = modPath;
+            ArcShipSysPath = arcShipSysPath;
+            AudioSbcPath = audioSbcPath;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
