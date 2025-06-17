78f7f9c9978b6dc0c902b28abc579ccae5a71429
StrongBroArina2 <arina.lokhovskaya@mail.ru>
2025-06-17 19:59:33 +0300

    -

---
+++ SEENG_P_ModManager.cs
@@ -0,0 +1,12 @@
+﻿namespace SEENG_Core
+{
+    public class ModManager
+    {
+        public static WorkshopMod LastSelectedMod { get; set; }
+
+        public static void UpdateMod(WorkshopMod mod)
+        {/// i love skybox loader
+            LastSelectedMod = mod;
+        }
+    }
+}
